bb0037569e06b7f97d347322e20e38455d097be4
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-02-05 17:56:08 +1100

    flaps work

--- core/src/com/mnahm5/flappy/bird/clone/FlappyBird.java
+++ core/src/com/mnahm5/flappy/bird/clone/FlappyBird.java
@@ -6,24 +6,45 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import org.w3c.dom.Text;
+
 public class FlappyBird extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture background;
+
+    private Texture[] birds;
+    int flapState = 0;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
         background = new Texture("bg.png");
+        birds = new Texture[2];
+        birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
     }
 
     @Override
     public void render () {
+
+        if (flapState == 0) {
+            flapState = 1;
+        }
+        else {
+            flapState = 0;
+        }
+
         batch.begin();
         batch.draw(
                 background,
                 0, 0,
                 Gdx.graphics.getWidth(),
                 Gdx.graphics.getHeight()
+        );
+        batch.draw(
+                birds[flapState],
+                Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - birds[flapState].getHeight() / 2
         );
         batch.end();
     }
@@ -32,5 +53,7 @@ public class FlappyBird extends ApplicationAdapter {
     public void dispose () {
         batch.dispose();
         background.dispose();
+        birds[0].dispose();
+        birds[1].dispose();
     }
 }
